97c90725bd028ada571fef5565914f76b5c49095
raval21 <129948279+raval21@users.noreply.github.com>
2026-04-08 06:25:50 -0700

    fix port+route

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on ${PORT}`));
 
 const authRoutes = require("./routes/authRoutes");
 
