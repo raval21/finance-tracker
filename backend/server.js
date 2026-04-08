46a6465f53d1942d5fb7e330096e4c8d7f6adc16
raval21 <129948279+raval21@users.noreply.github.com>
2026-04-08 08:56:12 -0700

    fixed code

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,40 @@
-
-
-console.log("ENV VALUE:", process.env.MONGO_URI);
-const cors = require("cors");
-const express = require("express");
-const mongoose = require("mongoose");
-
 require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// Debug crash logs
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT ERROR:", err);
+});
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED PROMISE:", err);
+});
+
+// Middleware
 app.use(cors());
-
-// app.use(cors({
-//   origin:"https://finance-tracker-silk-alpha.vercel.app",methods:["GET","POST","PUT","DELETE"], allowedHeaders:["Content-Type","Authorization"],credentials:true
-// }));
-
 app.use(express.json());
 
 // Test route
 app.get("/", (req, res) => {
-  res.send("API running...");
+  res.send("Backend running ✅");
 });
 
-// DB Connection
+// Routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+
+// DB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log("Mongo Error:", err));
 
-
-
-const authRoutes = require("./routes/authRoutes");
-
-app.use("/api/auth", authRoutes);
-
-const transactionRoutes = require("./routes/transactionRoutes");
-
-app.use("/api/transactions", transactionRoutes);
-app.get("/", (req, res) => {
-res.send("backend running...");
-});
-
 // Server
-const PORT = process.env.PORT || 5000 ;
-app.listen(PORT, "0.0.0.0", () => console.log(`Server running on ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
+});
